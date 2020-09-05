c665ce8e9585ad2e03b82b9fe939dbb25328b1fe
Arthur <neonbetcy@gmail.com>
2020-09-05 10:56:37 +0300

    Revert "Create actions, constants and other features."
    This reverts commit 6dbe6d6cf9969a524d263e5c5f2c9fc5eca1cd6b.

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { postsBySubreddit } from "./postsBySubredit";
-import { selectedSubreddit } from "./selectedSubreddit";
-
-export const rootReducer = combineReducers({
-  selectedSubreddit,
-  postsBySubreddit,
-});
